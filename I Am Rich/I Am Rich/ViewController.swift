d4807c83ddbcdd518bee48bff809e3794b696f68
Ice21 <35847819+Ice2192@users.noreply.github.com>
2018-08-31 23:23:44 -0500

    Add files via upload

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Kevin Alonzo on 8/29/18.
+//  Copyright © 2018 Kevin Alonzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
