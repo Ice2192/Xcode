323084feb060bd3bcc15c8de038088dd13ff6633
Kevin <kalonzo21@gmail.com>
2018-08-31 23:07:44 -0500

    ok

---
+++ Variables, Constants, and Data Types.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myAge : Int = 25
+
+myAge = 26
+
+let myName : String = "Kevn"
+
+let myAgeInTenYears = myAge + 10
+
+let myDetails = "\(myName), \(myAge)"
+
+let wholeNumbers : Int = 12
+let  text : String = "abc"
+
+let booleans : Bool = true
+
+let floatingPointNumber : Float = 1.3
+
+let double : Double = 3.14
